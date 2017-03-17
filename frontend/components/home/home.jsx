eb444f7228cd2bff33bc1325c31cb5e34d70cf45
fafafariba <fafafariba@Ordi.local>
2017-03-16 23:22:16 -0700

    Start building user home page

--- frontend/components/home/home.jsx
+++ frontend/components/home/home.jsx
@@ -1,29 +1,48 @@
 import React from 'react';
-
+import Subscriptions from '../subscriptions/subscriptions';
+import { Link } from 'react-router';
 
 const Home = (props) => {
   return (
     <main className="home">
       <section>
-        <p>Featured</p>
+        <h2>Featured</h2>
         <ul>
-          <li>Podcast 1</li>
-          <li>Podcast 2</li>
-          <li>Podcast 3</li>
+          <li>
+            <Link to="/podcasts">
+              <img src="http://cloudfront.assets.stitcher.com/feedimagesplain328/56516.jpg"/>
+            </Link>
+            <p>Reply All</p>
+          </li>
+          <li>
+            <Link to="/podcasts">
+              <img src="http://cloudfront.assets.stitcher.com/feedimagesplain328/54050.jpg"/>
+            </Link>
+            <p>Serial</p>
+          </li>
+          <li>
+            <Link to="/podcasts">
+              <img src="http://cloudfront.assets.stitcher.com/feedimagesplain328/85049.jpg"/>
+            </Link>
+            <p>How to Be a Girl</p>
+        </li>
+          <li>
+            <Link to="/podcasts">
+              <img src="http://cloudfront.assets.stitcher.com/feedimagesplain328/11394.jpg"/>
+            </Link>
+          <p>Freakonomics</p>
+          </li>
+
         </ul>
       </section>
       <br/>
       <section>
-        <p>My Subscriptions</p>
-        <ul>
-          <li>Podcast 1</li>
-          <li>Podcast 2</li>
-          <li>Podcast 3</li>
-        </ul>
+        <h2>My Subscriptions</h2>
+        <Subscriptions />
       </section>
       <br/>
       <section>
-        <p>Popular</p>
+        <h2>Popular</h2>
         <ul>
           <li>Podcast 1</li>
           <li>Podcast 2</li>
---
+++ frontend/components/subscriptions/subscriptions.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+class Subscriptions extends React.Component {
+  constructor(props){
+    super(props);
+  }
+
+  render() {
+    return (
+      <section>
+        <ul>
+          <li>
+            hi
+          </li>
+        </ul>
+      </section>
+    );
+  }
+}
+
+
+export default Subscriptions;
