666008f0a21b93d991f084044b338bc55edaa707
fafafariba <fafafariba@Ordi.local>
2017-03-20 09:52:56 -0700

    Fix validation error

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -1,6 +1,6 @@
 class Episode < ApplicationRecord
   validates :name, :description, :podcast_id, :audio_url, :date, :no,
-    :duration, :image_url
+    :duration, :image_url, presence: true
 
   belongs_to :podcast
 end
